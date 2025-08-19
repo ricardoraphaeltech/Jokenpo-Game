d96b294960f8112b536076c286a4bf365db9dd94
ricardoraphaeltech <riraphaelusa@gmail.com>
2025-08-19 18:54:24 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let yrScore = 0;
+let opntScore = 0;
+
+function play(playerChoice) {
+    const finalResult = document.querySelector(".final-result");
+    const yourScore = document.getElementById("your-scores");
+    const oponentScore = document.getElementById("oponent-scores");
+
+    const options = ["pedra", "papel", "tesoura"];
+    const oponentChoice = options[Math.floor(Math.random() * 3)];
+
+    let result = "";
+
+    if (playerChoice === oponentChoice) {
+        result = "Empate!";
+    } else if (
+        (playerChoice === "pedra" && oponentChoice === "tesoura") ||
+        (playerChoice === "papel" && oponentChoice === "pedra") ||
+        (playerChoice === "tesoura" && oponentChoice === "papel")
+    ) {
+        result = "Você venceu!";
+        yrScore++;
+    } else {
+        result = "Você perdeu!";
+        opntScore++;
+    }
+
+    finalResult.innerHTML = `Você escolheu ${playerChoice} e
+    o Oponente escolheu ${oponentChoice}. ${result}`;
+
+    yourScore.innerHTML = yrScore;
+    oponentScore.innerHTML = opntScore;
+};
